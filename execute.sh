3483589790ca5b59f6479737642caac5b2187372
jchaverot <jeremy.chaverot@epfl.ch>
2024-01-08 19:01:29 +0100

    update bash script and utils

--- execute.sh
+++ execute.sh
@@ -2,20 +2,22 @@
 #SBATCH --chdir /scratch/izar/jchavero
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu_free
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
-#SBATCH --time=1:0:0
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem 16G
-#SBATCH --account cs307
-#SBATCH --reservation cs307-gpu
 
 echo STARTING AT `date`
 
+echo "Loading modules"
 module load gcc openmpi py-torch py-torchvision cuda
+
+echo "Launching the Virtual Environment"
 source ~/opt/izar1/venv-gcc/bin/activate
+
+echo "Navigating to the directory and executing the task"
 cd ~/Gen6D                                    
 python eval.py --cfg configs/gen6d_pretrain.yaml --object_name spacecraft/hubble
 
-echo FINISHED at `date`
+echo FINISHED AT `date`
